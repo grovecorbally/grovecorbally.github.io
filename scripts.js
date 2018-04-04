965306f5e5ac459319a6b8d8c9945272a0007674
Conor Creagh <creaghstc@gmail.com>
2018-04-04 21:39:21 +0100

    Fixed height issue with trip advisor

--- scripts.js
+++ scripts.js
@@ -31,3 +31,7 @@
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
   $("#twitter, #tripadvisor").hide();
 }
+
+if(/chrome/i.test(navigator.userAgent)){
+  $("#twitter, #facebook").addClass("chrome");
+}
