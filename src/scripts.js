78fdf96855c8bdd5f21663fcba2098cfdeef47ed
Conor Creagh <creaghstc@gmail.com>
2018-04-05 23:50:31 +0100

    Structure, food menu and about us updated

---
+++ src/scripts.js
@@ -0,0 +1,37 @@
+// const finger = new Fingerprint().get();
+// const form = "<div class='col-md-5'><input class='textbox'placeholder='Enter title' id='food-title'></input><input class='textbox'placeholder='Enter description' id='food-description'><button class='btn btn-success' id='add-food'>add Food</button></input></div>";
+// var html = "";
+// console.log(finger);
+//
+// $("#admin-login").on("click", function() {
+//   verify(prompt("Enter admin login key"));
+// });
+//
+//
+// function verify(code) {
+//   if (parseInt(code) === finger) {
+//     $("#forms").show();
+//     $("#edit").html(form);
+//
+//     $("#add-food").on("click", function() {
+//       html += "<h4>" + $("#food-title").val() + "</h4><p>" + $("#food-description").val() + "</p>";
+//       console.log(html);
+//     });
+//     $("#submit-menu").on("click", function() {
+//       ("#offset").html(html);
+//     })
+//
+//   } else {
+//     alert("Incorrect code");
+//   }
+//
+//
+// }
+
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+  $("#twitter, #tripadvisor").hide();
+}
+
+if(/chrome/i.test(navigator.userAgent)){
+  $("#twitter, #facebook").addClass("chrome");
+}
